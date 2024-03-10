8575bf3950b7eeeafe5b8f3f5c4732c692fdd397
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-03-10 02:25:51 +0000

    added notes for except and only section

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # industrial-auth-1
 
 Target: https://industrial-auth-1.matchthetarget.com/
+
+FLOW:
+
+1. Here are the to-do list that needs to be addressed and fixed in this project:
+  A. A user that does NOT own the photo should be redirected back to the same page when clicking on a delete button for a photo. Should hide the link for the delete button in this scenario.
+
+  B. Same should go for edit on a photo.
+
+  C. Same should be considered for deleting a comment that is not OWNED by the current_user. 
+
+  D. Same should go for the edit on a comment. 
+
+  E. For private accounts, the visiting user should only be able to see the FOLLOWING: followers and leaders. 
+
+  F. For private accounts, the POSTS and like section should not be shown unless the viewing user is an accepted follower.
+
+  G. If it's a public account then anyone can view their posts and likes even if they are not a follower of the public user account. 
+
+  H. Only a user should be able to see their own pending follow requests
+
+  -I. The collections of photos that a user sees through their leaders (feed/discover) should only be viewable by them. 
+
+2. Using except and only -> routes.rb
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :comments
+  #2 For comments, the only routes we're concerned about is the ability to create a comment, update it, or delete it. Therefore, we can use except to remove the rest.
+  resources :comments, except: [:index, :show, :new]
+  
+  #2 Specifically for the follow_requests, we are NOT interested in the GET routes of this route. Therefore, we use except to exclude all the GET routes. We still utilize the POST (:create), PATCH (:update), and DELETE (:delete).
   resources :follow_requests, except: [:index, :show, :new, :edit]
+
+  #2 For likes, we ONLY need to utilize the :create and :destroy routes. Why? because the likes only fuctions either as liking something (= :create) or unliking something (= :destroy).
   resources :likes, only: [:create, :destroy]
+
+  #2 For photos, we do NOT want to show all the photos inside of our database (which is what the Ruby logic is doing). Instead, we want to showcase the "discover" and "feed" section. Therefore, we use except to plug a whole with the index route - something we won't use.
   resources :photos, except: [:index]
 
   get ":username" => "users#show", as: :user
