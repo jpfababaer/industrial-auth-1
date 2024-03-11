d44c3d77ce33520abc95547b5dbf447b80b2bce4
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-03-11 20:52:16 +0000

    added authorization to User#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def show
+    authorize @user
+  end
+
   private
 
     def set_user
