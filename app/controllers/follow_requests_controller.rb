c2a288c9c22096482cd1be94b5edab2cc3629f88
John Paul Fababaer <100891759+jpfababaer@users.noreply.github.com>
2024-03-12 21:52:42 +0000

    did authorization for follow request

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -1,28 +1,34 @@
 class FollowRequestsController < ApplicationController
   before_action :set_follow_request, only: %i[ show edit update destroy ]
+  before_action {authorize @follow_request || FollowRequest }
 
   # GET /follow_requests or /follow_requests.json
   def index
     @follow_requests = FollowRequest.all
+
   end
 
   # GET /follow_requests/1 or /follow_requests/1.json
   def show
+
   end
 
   # GET /follow_requests/new
   def new
     @follow_request = FollowRequest.new
+
   end
 
   # GET /follow_requests/1/edit
   def edit
+
   end
 
   # POST /follow_requests or /follow_requests.json
   def create
     @follow_request = FollowRequest.new(follow_request_params)
     @follow_request.sender = current_user
+
 
     respond_to do |format|
       if @follow_request.save
@@ -38,6 +44,7 @@ class FollowRequestsController < ApplicationController
   # PATCH/PUT /follow_requests/1 or /follow_requests/1.json
   def update
     respond_to do |format|
+
       if @follow_request.update(follow_request_params)
         format.html { redirect_back fallback_location: root_url, notice: "Follow request was successfully updated." }
         format.json { render :show, status: :ok, location: @follow_request }
@@ -51,6 +58,7 @@ class FollowRequestsController < ApplicationController
   # DELETE /follow_requests/1 or /follow_requests/1.json
   def destroy
     @follow_request.destroy
+
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Follow request was successfully destroyed." }
       format.json { head :no_content }
---
+++ app/policies/follow_request_policy.rb
@@ -0,0 +1,28 @@
+class FollowRequestPolicy < ApplicationPolicy
+  attr_reader :user, :follow_request
+
+  def initialize(user, follow_request)
+    @user = user
+    @follow_request = follow_request
+  end
+
+  def index?
+    false
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user == follow_request.recipient || user == follow_request.sender
+  end
+
+  def destroy?
+    update?
+  end
+end
